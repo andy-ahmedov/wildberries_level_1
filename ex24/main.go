6b5a887d1ac0ebc4c18120316596dc958d9804d9
Andy <andy.ahmedov2@gmail.com>
2023-03-26 01:36:09 +0300

    add go.mod

--- ex24/main.go
+++ ex24/main.go
@@ -2,28 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-type Point struct {
-	x float64
-	y float64
-}
-
-func NewPoint(x float64, y float64) *Point {
-	return &Point{
-		x: x,
-		y: y,
-	}
-}
-
-func Distance(p1 *Point, p2 *Point) float64 {
-	result := math.Sqrt(math.Pow((p2.x-p1.x), 2) + math.Pow((p2.y-p1.y), 2))
-	return result
-}
-
 func main() {
-	p1 := NewPoint(2, 3)
+	p1 := NewPoint(5, 7)
 	p2 := NewPoint(5, 7)
 
 	dist := Distance(p1, p2)
--- ex24/structs/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/andy-ahmedov/wildberries_level_1/ex24/structs
-
-go 1.19
